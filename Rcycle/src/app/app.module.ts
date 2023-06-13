e8537575f9fca753e731006d2d2fba1f4b0d485e
Patricio Lagos <Pat.lagos@duocuc.cl>
2023-06-13 16:33:09 -0400

    1

--- Rcycle/src/app/app.module.ts
+++ Rcycle/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { LOCALE_ID } from '@angular/core';
 import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
 import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Plugin } from '@awesome-cordova-plugins/core';
 
 registerLocaleData(localeEs);
 
@@ -21,4 +22,6 @@ registerLocaleData(localeEs);
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, {provide: LOCALE_ID, useValue:"es"}, SQLite],
   bootstrap: [AppComponent],
 })
+
+
 export class AppModule {}
