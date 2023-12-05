78e09eedf276b273bb3e667905aeea2fed968f5b
Patricio Lagos <115107187+patricio97@users.noreply.github.com>
2023-12-05 19:31:15 -0300

    a

--- Rcycle/src/app/app.module.ts
+++ Rcycle/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { LOCALE_ID } from '@angular/core';
 import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
 import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Plugin } from '@awesome-cordova-plugins/core';
 
 registerLocaleData(localeEs);
 
